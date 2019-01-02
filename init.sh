50bec4866c4ac0651fbc6fb69fb47b643409bce8
jihyechoi77 <jihyec.0707@gmail.com>
2019-01-02 18:54:52 -0500

    initial setting

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PYTHONPATH=/data/jihyec/code/facenet-master/src
+export PYTHONPATH="/data/jihyec/adversarial-attack/src":$PYTHONPATH
+export PYTHONPATH="/data/jihyec/code/cleverhans-master/cleverhans":$PYTHONPATH
+export LD_LIBRARY_PATH=/usr/local/lib
+
+# to fix ValueError: unknown locale: UTF-8 error when importing matplotlib
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
