edf68693ee04829d049fae9fa5645ab87451df81
jihyechoi77 <jihyec.0707@gmail.com>
2019-01-06 09:00:05 -0500

    now automatically creates corresponding save directories

--- misc/resize_image.py
+++ misc/resize_image.py
@@ -13,7 +13,9 @@ data_dir = sys.argv[1]
 save_dir = sys.argv[2]
 # new_dim = [int(sys.argv[2])]  # for example, 224
 new_dim = 160
-cropped = 45
+cropped = 0 #45
+
+os.mkdir(save_dir)
 
 # for celeba
 """
@@ -35,9 +37,12 @@ print("Completed")
 """
 
 # for lfw / vgg
-for ith_sbj_name in os.listdir(data_dir):
+sbj_names = [o for o in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir,o))]
+for ith_sbj_name in sbj_names:
   print("Processing %s" % ith_sbj_name)
   data_dir_full = os.path.join(data_dir, ith_sbj_name)
+  save_dir_full = os.path.join(save_dir, ith_sbj_name)
+  os.mkdir(save_dir_full)
 
   for file_name in os.listdir(data_dir_full):
     image_path = os.path.join(data_dir_full, file_name)
@@ -51,5 +56,5 @@ for ith_sbj_name in os.listdir(data_dir):
     output = image.resize((new_dim, new_dim), Image.ANTIALIAS)
 
     # output_file_name = os.path.join(os.path.join(save_dir, ith_sbj_name), file_name)
-    output_file_name = os.path.join(os.path.join(save_dir, ith_sbj_name), file_name)
+    output_file_name = os.path.join(save_dir_full, file_name)
     output.save(output_file_name, "JPEG", quality=95)
