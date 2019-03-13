97199968a6bc129f2153a6db33e65b93be0e3b20
jihyechoi77 <jihyec.0707@gmail.com>
2019-03-13 18:06:24 -0400

    added image array visualization

--- src/attack_utils.py
+++ src/attack_utils.py
@@ -10,12 +10,12 @@ def evaluate(true_labels, real_labels, adversarial_labels):
     different_faces_index = np.where((true_labels == 1))
 
     accuracy = np.mean(true_labels[same_faces_index[0]] == adversarial_labels[same_faces_index[0]])
-    print('Number of same person faces: ' + str(sum(same_faces_index[0])))
+    print('Number of same person faces: ' + str(len(same_faces_index[0])))
     print('Accuracy against adversarial examples for same person faces (dodging): '
           + str(accuracy * 100) + '%')
 
     accuracy = np.mean(true_labels[different_faces_index[0]] == adversarial_labels[different_faces_index[0]])
-    print('Number of different people faces: ' + str(sum(different_faces_index[0])))
+    print('Number of different people faces: ' + str(len(different_faces_index[0])))
     print('Accuracy against adversarial examples for different people faces (impersonation): '
           + str(accuracy * 100) + '%')
 
